37775192a22710ab9ff59a832870617e8eef10b2
Alexander Rohrig <me@alexanderrohrig.com>
2024-07-25 10:39:16 -0400

    Added isiPad

--- Sources/Core/RohrigSoftwareCompanyCore.swift
+++ Sources/Core/RohrigSoftwareCompanyCore.swift
@@ -1,10 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import Foundation
+#if canImport(UIKit)
+import UIKit
+#endif
 
 public class RSCCore {
     public static let shared = RSCCore()
-    private init() {}
+    
     /// Standard rounded corner size
     public let roundedCornerSize = CGSize(width: 25, height: 25)
+    
+    public var isiPad: Bool {
+        #if os(iOS)
+        UIDevice.current.userInterfaceIdiom == .pad {
+            return true
+        }
+        #endif
+        return false
+    }
+    
+    private init() {}
 }
