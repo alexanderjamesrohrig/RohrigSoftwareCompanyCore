3029a1cda454eecb4972ef2abd6dffda7ce9d463
Alexander Rohrig <me@alexanderrohrig.com>
2024-07-26 11:08:27 -0400

    Added version and build strings

--- Sources/Core/RohrigSoftwareCompanyCore.swift
+++ Sources/Core/RohrigSoftwareCompanyCore.swift
@@ -8,9 +8,13 @@ import UIKit
 public class RSCCore {
     public static let shared = RSCCore()
     
-    /// Standard rounded corner size
+    private let versionKey = "CFBundleShortVersionString"
+    private let buildKey = "CFBundleVersion"
+    
+    /// Standard rounded corner size.
     public let roundedCornerSize = CGSize(width: 25, height: 25)
     
+    /// `Bool` that is true when user is using an iPad.
     public var isiPad: Bool {
         #if os(iOS)
         if UIDevice.current.userInterfaceIdiom == .pad {
@@ -18,6 +22,18 @@ public class RSCCore {
         }
         #endif
         return false
+    }
+    /// `String` version number.
+    public var version: String {
+        let dictionary = Bundle.main.infoDictionary
+        let version = dictionary?[versionKey] as? String
+        return version ?? "nil"
+    }
+    /// `String` build number.
+    public var build: String {
+        let dictionary = Bundle.main.infoDictionary
+        let build = dictionary?[buildKey] as? String
+        return build ?? "nil"
     }
     
     private init() {}
