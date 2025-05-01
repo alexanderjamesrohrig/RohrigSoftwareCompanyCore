056a44d8bd594a470a0509a706014500004c54a6
Alexander Rohrig <me@alexanderrohrig.com>
2025-05-01 18:28:19 -0400

    Added osVersion

--- Sources/Core/RohrigSoftwareCompanyCore.swift
+++ Sources/Core/RohrigSoftwareCompanyCore.swift
@@ -1,9 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import Foundation
-#if canImport(UIKit)
-import UIKit
-#endif
 
 public class RSCCore {
     public static let shared = RSCCore()
@@ -14,21 +11,13 @@ public class RSCCore {
     /// Standard rounded corner size.
     public let roundedCornerSize = CGSize(width: 25, height: 25)
     
-    /// `Bool` that is true when user is using an iPad.
-    public var isiPad: Bool {
-        #if os(iOS)
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            return true
-        }
-        #endif
-        return false
-    }
     /// `String` version number.
     public var version: String {
         let dictionary = Bundle.main.infoDictionary
         let version = dictionary?[versionKey] as? String
         return version ?? "nil"
     }
+    
     /// `String` build number.
     public var build: String {
         let dictionary = Bundle.main.infoDictionary
@@ -38,3 +27,29 @@ public class RSCCore {
     
     private init() {}
 }
+
+// MARK: UIKit
+#if canImport(UIKit)
+import UIKit
+
+extension RSCCore {
+    /// `Bool` that is true when user is using an iPad.
+    public var isiPad: Bool { UIDevice.current.userInterfaceIdiom == .pad }
+    
+    /// `String` that describes the current OS version
+    public var osVersion: String { UIDevice.current.systemVersion }
+}
+#endif
+
+// MARK: AppKit
+#if canImport(AppKit)
+import AppKit
+
+extension RSCCore {
+    /// `Bool` that is true when user is using an iPad.
+    public var isiPad: Bool { false }
+    
+    /// `String` that describes the current OS version
+    public var osVersion: String { ProcessInfo.processInfo.operatingSystemVersionString }
+}
+#endif
